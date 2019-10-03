f2e53195b05a69aec7a8c1a99da81bf995d859f6
Priscila Araujo <araujo16012002@gmail.com>
2019-10-03 17:55:11 -0300

    Adcionar contador

--- src/componentes/Placar.js
+++ src/componentes/Placar.js
@@ -1,11 +1,15 @@
-import React from "react";
-import './Placar.scss'
+import React,{ useState } from "react";
+import './Placar.scss';
 
-    
+
 const Placar = ({time}) => {
+    const [votos, setVotos] = useState(0);
+    
     return (
     <div className="Placar">
         <h1>{time}</h1>
+        <h3>{votos}</h3>
+        
     </div>
     )
 }
