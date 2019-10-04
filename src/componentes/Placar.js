06ce281da876e6354e77701dc32af01c91c3900c
Priscila Araujo <araujo16012002@gmail.com>
2019-10-04 15:13:31 -0300

    Adicionar regras do contador e estilo dos botões

--- src/componentes/Placar.js
+++ src/componentes/Placar.js
@@ -4,12 +4,25 @@ import './Placar.scss';
 
 const Placar = ({time}) => {
     const [votos, setVotos] = useState(0);
-    
+
+    const incrementar = () => {
+        setVotos(votos + 1);
+    }
+
+    const decrementar = () => {
+        if (votos > 0) {
+            setVotos(votos - 1);
+        }
+    }
+
     return (
     <div className="Placar">
         <h1>{time}</h1>
         <h3>{votos}</h3>
-        
+        <div>
+            <button onClick={decrementar}>-</button>
+            <button onClick={incrementar}>+</button>
+        </div>
     </div>
     )
 }
